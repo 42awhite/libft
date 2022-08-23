8161b9be823c517436f2be765a34a9dc2aff4359
Ana Blanco Mota <ablanco-@c2r7s2.42madrid.com>
2022-08-23 16:50:55 +0200

    strtrim HECHO

--- Makefile
+++ Makefile
@@ -6,7 +6,7 @@
 #    By: ablanco- <marvin@42.fr>                    +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2022/06/21 15:28:47 by ablanco-          #+#    #+#              #
-#    Updated: 2022/08/20 15:51:07 by ablanco-         ###   ########.fr        #
+#    Updated: 2022/08/23 16:42:18 by ablanco-         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -14,7 +14,7 @@ SRCS	= ft_isalpha.c ft_isdigit.c ft_isalnum.c ft_isascii.c ft_isprint.c \
 		  ft_strlen.c ft_memset.c ft_bzero.c ft_memcpy.c ft_memmove.c  ft_strlcpy.c \
 		  ft_strlcat.c ft_toupper.c ft_tolower.c ft_strchr.c ft_strrchr.c ft_strncmp.c \
 		  ft_memchr.c ft_memcmp.c ft_strnstr.c ft_atoi.c ft_calloc.c ft_strdup.c ft_substr.c \
-		  ft_strjoin.c
+		  ft_strjoin.c ft_strtrim.c
 
 OBJS	= ${SRCS:.c=.o}
 
--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,7 +6,7 @@
 /*   By: ablanco- <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/20 16:02:05 by ablanco-          #+#    #+#             */
-/*   Updated: 2022/08/22 14:16:06 by ablanco-         ###   ########.fr       */
+/*   Updated: 2022/08/23 16:48:11 by ablanco-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "libft.h"
@@ -35,6 +35,8 @@ char *ft_strtrim(char const *s1, char const *set)
 	size_t end;
 	size_t cont;
 	
+	if(!s1)
+		return (0);
 	start = 0;
 	while (s1[start] && ft_poschar(s1[start], set))
 		start++;
@@ -55,7 +57,7 @@ char *ft_strtrim(char const *s1, char const *set)
 	return (str);
 }
 
-
+/*
 int main(void)
 {
 	char s1[] = "aaaaaaaaaataaaaaaaa";
@@ -64,3 +66,4 @@ int main(void)
 	printf("%s", ft_strtrim(s1, set));
 	return (0);
 }
+*/
