f4669c87227765bb4c0f2c5ba36a9f9570ac7bd0
PC <pc@MacBook-Air-de-PC.local>
2022-08-24 12:52:30 +0200

    vamos a llorar con split

--- ft_split.c
+++ ft_split.c
@@ -9,6 +9,23 @@
 /*   Updated: 2022/08/23 16:54:55 by ablanco-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+#include "libft.h"
+
+
+static int ft_nchar(char *s1)
+{
+	size_t lenght;
+	int cont;
+	
+	cont = 0;
+	length = 0;
+	while (s1[cont] != '\0')
+		{
+			length ++;
+			cont ++;
+		}
+	return (lenght);
+}
 
 char **ft_split(char const *s, char c)
 {
