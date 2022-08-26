a128c5020712da601cddbdf4df40ecebf7ebcf52
PC <pc@MacBook-Air-de-PC.local>
2022-08-26 10:44:44 +0200

    intentando split

--- ft_split.c
+++ ft_split.c
@@ -10,16 +10,47 @@
 /*                                                                            */
 /* ************************************************************************** */
 #include "libft.h"
+#include <stdio.h>
+#include <string.h>
 
+static int ft_nstr(char *s1, char c)
+{
+	size_t cont;
+	size_t ns;
+	
+	ns = 0;
+	cont = 0;
+	while(s1[cont] != '\0')
+		{
+			if (s1[cont] != c)
+				cont++;
+			else
+			{
+				ns++;
+				cont++;
+			}
+		}
+	ns++;
+	return(ns);
+}
 
-static int ft_nchar(char *s1)
+int main(void)
+{
+	char s1[] = "hola que tal";
+	char c = ' ';
+
+	printf ("%d\n", ft_nstr(s1, c));
+}
+
+/*
+static int ft_nchar(char *s2)
 {
 	size_t lenght;
 	int cont;
 	
 	cont = 0;
 	length = 0;
-	while (s1[cont] != '\0')
+	while (s2[cont] != '\0')
 		{
 			length ++;
 			cont ++;
@@ -31,3 +62,4 @@ char **ft_split(char const *s, char c)
 {
 
 }
+*/
